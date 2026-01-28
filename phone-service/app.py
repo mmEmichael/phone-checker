105d96a135de1e716be1799f590932ce05b2d3c2
michael <mm.e.mihon@gmail.com>
2026-01-28 12:12:40 +0300

    phonenumbers

--- phone-service/app.py
+++ phone-service/app.py
@@ -1,5 +1,7 @@
 import redis.asyncio as redis
 import asyncio
+import phonenumbers
+from phonenumbers import carrier, geocoder
 
 redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
@@ -12,8 +14,20 @@ async def phone_service():
 
         await redis_client.set(f"task:{task_id}:status", "processing") # статус: Начали обработку
 
-        async for phone, reg in redis_client.hscan_iter(f"task:{task_id}:phones"): # переделать это место!!! Здесь вызывать функцию определения региона и оператора
-            pass
+        async for phone, data in redis_client.hscan_iter(f"task:{task_id}:phones"): # переделать это место!!! Здесь вызывать функцию определения региона и оператора
+            # если номер без плюча, добавляем
+            if phone[0] != "+":
+                plus_phone = "+" + phone
+            else:
+                plus_phone = phone
+            
+            parsed_phone = phonenumbers.parse(plus_phone, None)
+            country = geocoder.country_name_for_number(parsed_phone, "en")
+            operator = carrier.name_for_number(parsed_phone, "en")
+            data = country + ": " + operator
+
+            #записываем данные 
+            await redis_client.hset(f"task:{task_id}:phones", phone, data)
 
         await redis_client.set(f"task:{task_id}:status", "processed") # статус: Закончили обработку
 
